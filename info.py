86ee491c2d8e1e0186c64b1b9329eb5fe8fdfd25
Eldorado-k <abdoulmadjid22k@gmail.com>
2025-07-24 14:52:02 +0000

    Update info.py

--- info.py
+++ info.py
@@ -1,5 +1,5 @@
-API_ID       = 250
-API_HASH     = "30db27938db32"
+API_ID       = 25926022
+API_HASH     = "30db27d9e56d854fb5e943723268db32"
 BOT_TOKEN    = "7703477452:AAFwFWWqN6xjRpWLJXsFyMSm7RyjD_WISAk"
 SESSION      = ""
 DATABASE_URI = "mongodb+srv://tgbot:4KzEdxEl4YldwwFR@tg.vr8ef.mongodb.net/?retryWrites=true&w=majority&appName=Tg"
