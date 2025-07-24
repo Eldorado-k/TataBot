fcb92a154803b8246d722ef09e2dbc11f9b596e7
Eldorado-k <abdoulmadjid22k@gmail.com>
2025-07-24 14:23:26 +0000

    Add files via upload

---
+++ info.py
@@ -0,0 +1,8 @@
+API_ID       = 25926022
+API_HASH     = "30db27d9e56d854fb5e943723268db32"
+BOT_TOKEN    = "7703477452:AAFwFWWqN6xjRpWLJXsFyMSm7RyjD_WISAk"
+SESSION      = ""
+DATABASE_URI = "mongodb+srv://tgbot:4KzEdxEl4YldwwFR@tg.vr8ef.mongodb.net/?retryWrites=true&w=majority&appName=Tg"
+LOG_CHANNEL  = -1002698474966
+ADMIN        = 8140299716
+CHANNEL      = "ZeeXClub"
---
+++ main.py
@@ -0,0 +1,4 @@
+from client import Bot
+
+print("Bot Started. Updates @ZeeXClub")
+Bot().run()
